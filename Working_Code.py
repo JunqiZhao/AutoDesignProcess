9f6a943410cd75ca18bafa83ae0d94b8f4689466
Junqi Zhao <gcgl09zjq@gmail.com>
2019-06-24 11:39:18 -0400

    Update Working_Code.py

--- Working_Code.py
+++ Working_Code.py
@@ -78,3 +78,46 @@ if None == errorReport:
     OUT = 0
 else:
     OUT = errorReport
+    
+    
+    
+    
+'''Try to Simplify the Turn on/off funcationality script'''
+
+# Set View Properties (Code: https://forum.dynamobim.com/t/setting-view-template-includes/15969)
+import clr
+# Import Element wrapper extension methods
+clr.AddReference("RevitNodes")
+import Revit
+clr.ImportExtensions(Revit.Elements)
+# Import DocumentManager and TransactionManager
+clr.AddReference("RevitServices")
+import RevitServices
+from RevitServices.Persistence import DocumentManager
+from RevitServices.Transactions import TransactionManager
+doc = DocumentManager.Instance.CurrentDBDocument
+# Import RevitAPI
+clr.AddReference("RevitAPI")
+from Autodesk.Revit.DB import *
+import System
+from System import Array
+from System.Collections.Generic import *
+TransactionManager.Instance.EnsureInTransaction(doc)
+
+# Batch setup the view template properties
+for i in IN[0]: #list of view templates
+	    	#update = False
+            #get all the parameters from current view template
+	    	vt = UnwrapElement(i)
+	    	allParams = [id.IntegerValue for id in vt.GetTemplateParameterIds()]
+            #turn off the view templates in the code block
+	    	focus = set(IN[1])
+	    	toSet = []
+	    	for j in allParams:
+	    		if j in focus:# focus only on the specified templates to turn off (with others turned on)
+	    			toSet.append(ElementId(j))
+	    	sysList = List[ElementId](toSet)
+	    	vt.SetNonControlledTemplateParameterIds(sysList) #This API will turn off the specified parameters
+TransactionManager.Instance.TransactionTaskDone()
+# Assign your output to the OUT variable
+OUT=0
